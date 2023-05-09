40c4eea070146e021f589614e380b2cd5480400d
Davor Roman <davoroman01@gmail.com>
2023-05-09 21:59:57 +0200

    Add next dependencie

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './User/user.module';
 import { ProductModule } from './product/product.module';
-import { UserCustomerModule } from './user-customer/user-customer.module';
+//import { UserCustomerModule } from './user-customer/user-customer.module';
 @Module({
-  imports: [UserModule, ProductModule, UserCustomerModule],
+  imports: [UserModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
