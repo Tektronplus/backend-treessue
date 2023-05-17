fa4c27345e683afeaf0583aa5f867580ab09adda
Davor Roman <davoroman01@gmail.com>
2023-05-17 15:43:30 +0200

    Fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'https://backend-treessue.vercel.app',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
