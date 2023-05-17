45842a791c75eca9a42789dfb5a41a955b9212c7
Davor Roman <davoroman01@gmail.com>
2023-05-17 15:35:29 +0200

    Const to let

--- src/user-login/user-login.controller.ts
+++ src/user-login/user-login.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Get, UseGuards, Post, Req,  Headers} from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Req, Headers } from '@nestjs/common';
 import { UserLoginService } from './user-login.service';
 import { ApiKeyAuthGuard } from '../auth/guard/apikey-auth.guard';
-import { AuthService } from "../auth/auth.service"
+import { AuthService } from '../auth/auth.service';
 import { UserCustomerService } from '../user-customer/user-customer.service';
 import { UserWorkerService } from '../user_worker/user_worker.service';
-import * as moment from "moment";
-import * as bcrypt from "bcrypt";
+import * as moment from 'moment';
+import * as bcrypt from 'bcrypt';
 import { Base64 } from 'js-base64';
-
 
 @UseGuards(ApiKeyAuthGuard)
 @Controller('user-login')
 export class UserLoginController {
-  constructor(private readonly userLoginService: UserLoginService, private authService:AuthService) {}
+  constructor(
+    private readonly userLoginService: UserLoginService,
+    private authService: AuthService,
+  ) {}
 
   @Get('/')
   async getHello(): Promise<string> {
@@ -29,103 +31,101 @@ export class UserLoginController {
     return this.userLoginService.findAllCustomer();
   }
 
-  @Post("/login")
-  async login(@Req() req, @Headers() headers)
-  {
+  @Post('/login')
+  async login(@Req() req, @Headers() headers) {
     type User = {
-      username?: string,
-      role?:string,
-    }
+      username?: string;
+      role?: string;
+    };
 
-    let headersData = headers.data
-    let data = Base64.decode(headersData)
-    console.log({data})
-    try{
-      const user:User = await this.userLoginService.findUser(data.split(":")[0],data.split(":")[1])
-      console.log({user})
-      return this.authService.generateUserToken(user)
+    const headersData = headers.data;
+    const data = Base64.decode(headersData);
+    console.log({ data });
+    try {
+      const user: User = await this.userLoginService.findUser(
+        data.split(':')[0],
+        data.split(':')[1],
+      );
+      console.log({ user });
+      return this.authService.generateUserToken(user);
+    } catch (err) {
+      console.log({ err });
+      return { error: err.message };
     }
-    catch(err)
-    {
-      console.log({err})
-      return {error:err.message}
-    } 
   }
 
-  @Post("/delete")
-  async deleteUser(@Req() req)
-  {
+  @Post('/delete')
+  async deleteUser(@Req() req) {
     type User = {
-      username?: string,
-      role?:string,
-    }
+      username?: string;
+      role?: string;
+    };
 
-    try{
-      const user:User = await this.userLoginService.findUser(req.body.username,req.body.password)
-      console.log({user})
-      return this.authService.generateUserToken(user)
+    try {
+      const user: User = await this.userLoginService.findUser(
+        req.body.username,
+        req.body.password,
+      );
+      console.log({ user });
+      return this.authService.generateUserToken(user);
+    } catch (err) {
+      console.log({ err });
+      return { error: err.message };
     }
-    catch(err)
-    {
-      console.log({err})
-      return {error:err.message}
-    } 
   }
-
-
 }
 
 @UseGuards(ApiKeyAuthGuard)
 @Controller('user-registration')
 export class UserRegisterController {
   constructor(
-    private readonly userCustomerService: UserCustomerService, 
-    private userWorker:UserWorkerService,
-    private userLoginService: UserLoginService
-    ) {}
+    private readonly userCustomerService: UserCustomerService,
+    private userWorker: UserWorkerService,
+    private userLoginService: UserLoginService,
+  ) {}
 
-  @Post("/registerCustomer")
-  async registerUser(@Req() req)
-  {
-    let newUser = req.body
-    console.log({newUser})
+  @Post('/registerCustomer')
+  async registerUser(@Req() req) {
+    const newUser = req.body;
+    console.log({ newUser });
     const saltOrRounds = 10;
     const salt = await bcrypt.genSalt(saltOrRounds);
-    const hash = await bcrypt.hash(newUser.password, salt); 
-    let userLoginEntity = {
-      userCustomer: "",
-      username:newUser.username,
-      password:hash,
-      role:"user"
-    }
-    let userCustomerEntity = {
-      first_name:newUser.firstName,
-      last_name:newUser.lastName,
-      birth_date:moment(newUser.birthDate,"DD-MM-YYYY").toDate(),
-      phone_number:newUser.phoneNumber,
-      email:newUser.email,
-      country:newUser.country,
-      province:newUser.province,
-      city:newUser.city,
-      zip_code:newUser.zipCode,
-      address:newUser.address
-    }
+    const hash = await bcrypt.hash(newUser.password, salt);
+    const userLoginEntity = {
+      userCustomer: '',
+      username: newUser.username,
+      password: hash,
+      role: 'user',
+    };
+    const userCustomerEntity = {
+      first_name: newUser.firstName,
+      last_name: newUser.lastName,
+      birth_date: moment(newUser.birthDate, 'DD-MM-YYYY').toDate(),
+      phone_number: newUser.phoneNumber,
+      email: newUser.email,
+      country: newUser.country,
+      province: newUser.province,
+      city: newUser.city,
+      zip_code: newUser.zipCode,
+      address: newUser.address,
+    };
 
-    try{
-      let newCreatedUserCustomer = await  this.userCustomerService.createUser(userCustomerEntity)
-      console.log({newCreatedUserCustomer})
-      userLoginEntity.userCustomer = newCreatedUserCustomer.id_user_customer
-      console.log({userLoginEntity})
-      let newCreatedUserLogin = await this.userLoginService.createUser(userLoginEntity)
-      console.log({newCreatedUserLogin})
-      return "utente creato con successo"
-      
-    }catch(err)
-    {
-      console.log(err)
-      return {error:err.message}
+    try {
+      const newCreatedUserCustomer = await this.userCustomerService.createUser(
+        userCustomerEntity,
+      );
+      console.log({ newCreatedUserCustomer });
+      userLoginEntity.userCustomer = newCreatedUserCustomer.id_user_customer;
+      console.log({ userLoginEntity });
+      const newCreatedUserLogin = await this.userLoginService.createUser(
+        userLoginEntity,
+      );
+      console.log({ newCreatedUserLogin });
+      return 'utente creato con successo';
+    } catch (err) {
+      console.log(err);
+      return { error: err.message };
     }
     //console.log({userLoginEntity},{userCustomerEntity})
   }
-
 }
