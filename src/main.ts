11bec957a17b37cd00bf6eefb42584ed636115c3
Davor Roman <davoroman01@gmail.com>
2023-05-17 15:51:01 +0200

    Fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   await app.listen(3000);
