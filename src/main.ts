3e7038df8e4163f0c6d1967514ae9a271beb5e27
Davor Roman <davoroman01@gmail.com>
2023-05-17 15:48:44 +0200

    Fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    allowedHeaders: ['content-type'],
     origin: '*',
     credentials: true,
   });
