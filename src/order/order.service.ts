2d11bbc993ef1c7bfa2ee76e25b737ffb2933f6b
Davor Roman <davoroman01@gmail.com>
2023-05-26 15:03:26 +0200

    Add random string method

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -12,3 +12,14 @@ export class OrderService {
     return this.OrderRepository.findAll();
   }
 }
+
+class CustomMethods {
+  randomString(length) {
+    const chars =
+      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = length; i > 0; --i)
+      result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+  }
+}
