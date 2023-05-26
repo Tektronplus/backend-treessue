d6d2d200d212de242053eb415ce5941c05cbfc83
Davor Roman <davoroman01@gmail.com>
2023-05-26 11:01:59 +0200

    Add product category endpoint

--- src/product-category/product-category.controller.ts
+++ src/product-category/product-category.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductCategoryService } from './product-category.service';
 import { ApiKeyAuthGuard } from '../auth/guard/apikey-auth.guard';
 
@@ -9,13 +19,43 @@ export class ProductCategoryController {
     private readonly productCategoryService: ProductCategoryService,
   ) {}
 
+  //--- CREATE ---
+  @Post('/create')
+  async createProductCategory(@Headers() headers, @Body() body): Promise<any> {
+    return this.productCategoryService.createNewProductCategory(headers, body);
+  }
+
+  //--- READ ---
   @Get('/')
   async getHello(): Promise<string> {
     return 'Hello from Product Category!';
   }
 
   @Get('/all')
-  async getListProductCategory(): Promise<Array<any>> {
-    return this.productCategoryService.findAll();
+  async getListProductCategory(@Headers() headers): Promise<Array<any>> {
+    return this.productCategoryService.findAll(headers);
+  }
+
+  //--- UPDATE ---
+  @Put('/update/:idProductCategory')
+  async updateProductCategoryById(
+    @Param() param,
+    @Headers() headers,
+    @Body() body,
+  ): Promise<any> {
+    return this.productCategoryService.updateProductCategoryById(
+      param.idProductCategory,
+      headers,
+      body,
+    );
+  }
+
+  //--- DELETE ---
+  @Delete('/delete/:idProductCategory')
+  async deleteProductById(@Param() param, @Headers() headers): Promise<any> {
+    return this.productCategoryService.deleteProductCategoryById(
+      param.idProductCategory,
+      headers,
+    );
   }
 }
--- src/product-category/product-category.module.ts
+++ src/product-category/product-category.module.ts
@@ -3,9 +3,10 @@ import { ProductCategoryController } from './product-category.controller';
 import { ProductCategoryService } from './product-category.service';
 import { productCategoryProvider } from './product-category.providers';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [ProductCategoryController],
   providers: [ProductCategoryService, ...productCategoryProvider],
   exports: [ProductCategoryService],
