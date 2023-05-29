68803e1e8ba67c0fae36653d146b8fabf9ea371b
Davor Roman <davoroman01@gmail.com>
2023-05-29 17:58:59 +0200

    Add update order

--- src/backOffice/backOffice_order.controller.ts
+++ src/backOffice/backOffice_order.controller.ts
@@ -1,14 +1,15 @@
 import {
-    Controller,
-    Get,
-    UseGuards,
-    Post,
-    Req,
-    Headers,
-    Res,
-    Body,
-    Param,
-  } from '@nestjs/common';
+  Controller,
+  Get,
+  UseGuards,
+  Post,
+  Req,
+  Headers,
+  Res,
+  Body,
+  Param,
+  Put,
+} from '@nestjs/common';
 
 import { OrderService } from '../order/order.service';
 import { ApiKeyAuthGuard } from '../auth/guard/apikey-auth.guard';
@@ -17,225 +18,247 @@ import { OrderStatusService } from 'src/order-status/order-status.service';
 import { UserCustomerService } from 'src/user-customer/user-customer.service';
 import { OrderDetailService } from '../order-detail/order-detail.service';
 import { ProductService } from '../product/product.service';
-  
 
+@UseGuards(ApiKeyAuthGuard)
+@Controller('backOfficeOrder')
+export class BackOfficeOrderController {
+  constructor(
+    private orderService: OrderService,
+    private userCustomerService: UserCustomerService,
+    private orderStatusService: OrderStatusService,
+    private orderDetailService: OrderDetailService,
+    private productService: ProductService,
+    private authService: AuthService,
+  ) {}
 
-  @UseGuards(ApiKeyAuthGuard)
-  @Controller('backOfficeOrder')
-  export class BackOfficeOrderController {
-    constructor(
-      private orderService:OrderService,
-      private userCustomerService:UserCustomerService,
-      private orderStatusService:OrderStatusService,
-      private orderDetailService:OrderDetailService,
-      private productService:ProductService,
-      private authService: AuthService
-    ) {}
-
-    @Get("/getAllOrder")
-    async getOrderList(@Headers() headers, @Res() res)
-    {
-
-        if(headers.authorization == undefined)
-        {
-          res.status(404).json({ result: 'bad request' });
-          return
-        }
-        if(headers.authorization.substring(0,7) != "Bearer ")
-        {
-          res.status(401).json({ result: 'not authorized' });
-          return
-        }
-    
-        let token = headers.authorization.split('Bearer ')[1];
-        console.log({ token });
-        const isTokenValid = await this.authService.validateToken(token);
-        if(isTokenValid)
-        {
-            const decodedInfo = await this.authService.dechiperUserToken(token);
-            console.log({ decodedInfo });
-            if (decodedInfo.userDetail.role == 'admin' || decodedInfo.userDetail.role == "ufficio")
-            {
-                try
-                {
-                    const orderList = await this.orderService.findAll() 
-                    for(let elm of orderList)
-                    {
-                        elm.order_status = await this.orderStatusService.findStatusById(elm.order_status)
-                        elm.order_status = elm.order_status.dataValues.status
-                        elm.user_customer = await this.userCustomerService.findOneById(elm.user_customer)
-                        elm.user_customer = elm.user_customer.dataValues.first_name + " " + elm.user_customer.dataValues.last_name
-                        console.log({elm})
-                    }
-                    res.status(200).json({ result: orderList });
-                    return
-                }
-                catch(err)
-                {
-                    res.status(500).json({ result: 'internal server error' });
-                    return
-                }
-            }
-            else
-            {
-                res.status(403).json({ result: 'not authorized' });
-                return
-            }
-
-        }
-        else
-        {
-            res.status(403).json({ result: 'not authorized' });
-            return
-        }
-
+  @Get('/getAllOrder')
+  async getOrderList(@Headers() headers, @Res() res) {
+    if (headers.authorization == undefined) {
+      res.status(404).json({ result: 'bad request' });
+      return;
+    }
+    if (headers.authorization.substring(0, 7) != 'Bearer ') {
+      res.status(401).json({ result: 'not authorized' });
+      return;
     }
 
-    @Get("/getOrderDetail/:id")
-    async getOrderDetail(@Param() param, @Headers() headers, @Res() res)
-    {
+    const token = headers.authorization.split('Bearer ')[1];
+    console.log({ token });
+    const isTokenValid = await this.authService.validateToken(token);
+    if (isTokenValid) {
+      const decodedInfo = await this.authService.dechiperUserToken(token);
+      console.log({ decodedInfo });
+      if (
+        decodedInfo.userDetail.role == 'admin' ||
+        decodedInfo.userDetail.role == 'ufficio'
+      ) {
+        try {
+          const orderList = await this.orderService.findAll();
+          for (let elm of orderList) {
+            elm.order_status = await this.orderStatusService.findStatusById(
+              elm.order_status,
+            );
+            elm.order_status = elm.order_status.dataValues.status;
+            elm.user_customer = await this.userCustomerService.findOneById(
+              elm.user_customer,
+            );
+            elm.user_customer =
+              elm.user_customer.dataValues.first_name +
+              ' ' +
+              elm.user_customer.dataValues.last_name;
+            console.log({ elm });
+          }
+          res.status(200).json({ result: orderList });
+          return;
+        } catch (err) {
+          res.status(500).json({ result: 'internal server error' });
+          return;
+        }
+      } else {
+        res.status(403).json({ result: 'not authorized' });
+        return;
+      }
+    } else {
+      res.status(403).json({ result: 'not authorized' });
+      return;
+    }
+  }
 
-        if(headers.authorization == undefined)
-        {
-          res.status(404).json({ result: 'bad request' });
-          return
-        }
-        if(headers.authorization.substring(0,7) != "Bearer ")
-        {
-          res.status(401).json({ result: 'not authorized' });
-          return
-        }
-    
-        let token = headers.authorization.split('Bearer ')[1];
-        console.log({ token });
-        const isTokenValid = await this.authService.validateToken(token);
-        if(isTokenValid)
-        {
-            const decodedInfo = await this.authService.dechiperUserToken(token);
-            console.log({ decodedInfo });
-            if (decodedInfo.userDetail.role == 'admin' || decodedInfo.userDetail.role == "ufficio")
-            {
-                try
-                {
-                    let orderDetail = await this.orderDetailService.findOrderDetail(param.id)
-                    orderDetail = orderDetail.map((data)=>{
-                        
-                        const orderDetail = {
-                            id_order_detail: data.dataValues.id_order_detail,
-                            id_order: data.dataValues.id_order,
-                            product: data.dataValues.id_product,
-                            price: data.dataValues.price,
-                            quantity: data.dataValues.quantity,
-                            description: data.dataValues.description
-                        }
-                        return orderDetail
-                    })
-                    for(let elm of orderDetail)
-                    {
-                        elm.product = await this.productService.findById(elm.product)
-                        elm.product = elm.product.dataValues.prod_name   
-                    }
-                    console.log({orderDetail})
-                    return orderDetail
-                }
-                catch(err)
-                {
-                    res.status(500).json({ result: 'internal server error' });
-                    return
-                }
-            }
-            else
-            {
-                res.status(403).json({ result: 'not authorized' });
-                return
-            }
-        }
-        else
-        {
-            res.status(403).json({ result: 'not authorized' });
-            return
-        }
+  @Get('/getOrderDetail/:id')
+  async getOrderDetail(@Param() param, @Headers() headers, @Res() res) {
+    if (headers.authorization == undefined) {
+      res.status(404).json({ result: 'bad request' });
+      return;
+    }
+    if (headers.authorization.substring(0, 7) != 'Bearer ') {
+      res.status(401).json({ result: 'not authorized' });
+      return;
     }
 
-    @Get()
-    async getOrderById(@Param() param, @Headers() headers, @Res() res)
-    {
-        if(headers.authorization == undefined)
-        {
-          res.status(404).json({ result: 'bad request' });
-          return
+    const token = headers.authorization.split('Bearer ')[1];
+    console.log({ token });
+    const isTokenValid = await this.authService.validateToken(token);
+    if (isTokenValid) {
+      const decodedInfo = await this.authService.dechiperUserToken(token);
+      console.log({ decodedInfo });
+      if (
+        decodedInfo.userDetail.role == 'admin' ||
+        decodedInfo.userDetail.role == 'ufficio'
+      ) {
+        try {
+          let orderDetail = await this.orderDetailService.findOrderDetail(
+            param.id,
+          );
+          orderDetail = orderDetail.map((data) => {
+            const orderDetail = {
+              id_order_detail: data.dataValues.id_order_detail,
+              id_order: data.dataValues.id_order,
+              product: data.dataValues.id_product,
+              price: data.dataValues.price,
+              quantity: data.dataValues.quantity,
+              description: data.dataValues.description,
+            };
+            return orderDetail;
+          });
+          for (let elm of orderDetail) {
+            elm.product = await this.productService.findById(elm.product);
+            elm.product = elm.product.dataValues.prod_name;
+          }
+          console.log({ orderDetail });
+          return orderDetail;
+        } catch (err) {
+          res.status(500).json({ result: 'internal server error' });
+          return;
         }
-        if(headers.authorization.substring(0,7) != "Bearer ")
-        {
-          res.status(401).json({ result: 'not authorized' });
-          return
-        }
-    
-        let token = headers.authorization.split('Bearer ')[1];
-        console.log({ token });
-        const isTokenValid = await this.authService.validateToken(token);
-        if(isTokenValid)
-        {
-            const decodedInfo = await this.authService.dechiperUserToken(token);
-            console.log({ decodedInfo });
-            if (decodedInfo.userDetail.role == 'admin' || decodedInfo.userDetail.role == "ufficio")
-            {
-                try
-                {
-                    const order = {
-                        id_order: "",
-                        user_customer: "",
-                        user_worker: null,
-                        order_status: "",
-                        order_date: "",
-                        courier_name: "",
-                        tracking_code: "",
-                        start_shipping_date: null,
-                        expected_delivery_date: "",
-                        delivery_date: null,
-                        orderList:[],
-                        price: ""
-                    }
-
-                    let orderDetail = await this.orderDetailService.findOrderDetail(param.id)
-                    orderDetail = orderDetail.map((data)=>{
-                        
-                        const orderDetail = {
-                            id_order_detail: data.dataValues.id_order_detail,
-                            id_order: data.dataValues.id_order,
-                            product: data.dataValues.id_product,
-                            price: data.dataValues.price,
-                            quantity: data.dataValues.quantity,
-                            description: data.dataValues.description
-                        }
-                        return orderDetail
-                    })
-                    for(let elm of orderDetail)
-                    {
-                        elm.product = await this.productService.findById(elm.product)
-                        elm.product = elm.product.dataValues.prod_name   
-                    }
-                    console.log({orderDetail})
-                    return orderDetail
-                }
-                catch(err)
-                {
-                    res.status(500).json({ result: 'internal server error' });
-                    return
-                }
-            }
-            else
-            {
-                res.status(403).json({ result: 'not authorized' });
-                return
-            }
-        }
-        else
-        {
-            res.status(403).json({ result: 'not authorized' });
-            return
-        }
+      } else {
+        res.status(403).json({ result: 'not authorized' });
+        return;
+      }
+    } else {
+      res.status(403).json({ result: 'not authorized' });
+      return;
     }
-  
+  }
 
+  @Get()
+  async getOrderById(@Param() param, @Headers() headers, @Res() res) {
+    if (headers.authorization == undefined) {
+      res.status(404).json({ result: 'bad request' });
+      return;
+    }
+    if (headers.authorization.substring(0, 7) != 'Bearer ') {
+      res.status(401).json({ result: 'not authorized' });
+      return;
+    }
+
+    const token = headers.authorization.split('Bearer ')[1];
+    console.log({ token });
+    const isTokenValid = await this.authService.validateToken(token);
+    if (isTokenValid) {
+      const decodedInfo = await this.authService.dechiperUserToken(token);
+      console.log({ decodedInfo });
+      if (
+        decodedInfo.userDetail.role == 'admin' ||
+        decodedInfo.userDetail.role == 'ufficio'
+      ) {
+        try {
+          const order = {
+            id_order: '',
+            user_customer: '',
+            user_worker: null,
+            order_status: '',
+            order_date: '',
+            courier_name: '',
+            tracking_code: '',
+            start_shipping_date: null,
+            expected_delivery_date: '',
+            delivery_date: null,
+            orderList: [],
+            price: '',
+          };
+
+          let orderDetail = await this.orderDetailService.findOrderDetail(
+            param.id,
+          );
+          orderDetail = orderDetail.map((data) => {
+            const orderDetail = {
+              id_order_detail: data.dataValues.id_order_detail,
+              id_order: data.dataValues.id_order,
+              product: data.dataValues.id_product,
+              price: data.dataValues.price,
+              quantity: data.dataValues.quantity,
+              description: data.dataValues.description,
+            };
+            return orderDetail;
+          });
+          for (let elm of orderDetail) {
+            elm.product = await this.productService.findById(elm.product);
+            elm.product = elm.product.dataValues.prod_name;
+          }
+          console.log({ orderDetail });
+          return orderDetail;
+        } catch (err) {
+          res.status(500).json({ result: 'internal server error' });
+          return;
+        }
+      } else {
+        res.status(403).json({ result: 'not authorized' });
+        return;
+      }
+    } else {
+      res.status(403).json({ result: 'not authorized' });
+      return;
+    }
+  }
+
+  @Put('/update/:idOrder')
+  async updateOrderById(
+    @Param() param,
+    @Headers() headers,
+    @Res() res,
+    @Body() body,
+  ): Promise<any> {
+    if (headers.authorization == undefined) {
+      res.status(404).json({ result: 'bad request' });
+      return;
+    }
+    if (headers.authorization.substring(0, 7) != 'Bearer ') {
+      res.status(401).json({ result: 'not authorized' });
+      return;
+    }
+
+    const token = headers.authorization.split('Bearer ')[1];
+    console.log({ token });
+    const isTokenValid = await this.authService.validateToken(token);
+    if (isTokenValid) {
+      const decodedInfo = await this.authService.dechiperUserToken(token);
+      console.log({ decodedInfo });
+      if (
+        decodedInfo.userDetail.role == 'admin' ||
+        decodedInfo.userDetail.role == 'ufficio'
+      ) {
+        try {
+          const result = await this.orderService.updateOrderById(
+            param.idOrder,
+            body,
+          );
+          console.log(result);
+          res.status(200).json({ result: 'Query executed successfully' });
+          return;
+        } catch (err) {
+          res.status(400).json({
+            result:
+              'Query error, please check your data. Probably, there is no difference between your data and the data that already exists.',
+          });
+          return;
+        }
+      } else {
+        res.status(403).json({ result: 'not authorized' });
+        return;
+      }
+    } else {
+      res.status(403).json({ result: 'not authorized' });
+      return;
+    }
+  }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Inject } from '@nestjs/common';
+import {
+  Injectable,
+  Inject,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { Order } from './order.entity';
 
 @Injectable()
@@ -11,24 +16,24 @@ export class OrderService {
   async findAll(): Promise<any> {
     const orderList = await this.OrderRepository.findAll();
     //console.log({orderList})
-    const orderData = await orderList.map((data)=>{
+    const orderData = await orderList.map((data) => {
       const orderData = {
-        id_order:data.dataValues.id_order,
-        user_customer:data.dataValues.id_user_customer,
-        user_worker:data.dataValues.id_user_worker,
-        order_status:data.dataValues.id_order_status,
-        order_date:data.dataValues.order_date,
-        courier_name:data.dataValues.courier_name,
-        tracking_code:data.dataValues.tracking_code,
-        start_shipping_date:data.dataValues.start_shipping_date,
-        expected_delivery_date:data.dataValues.expected_delivery_date,
-        delivery_date:data.dataValues.delivery_date,
-        price:data.dataValues.price
-      }
-      return orderData
-    })
+        id_order: data.dataValues.id_order,
+        user_customer: data.dataValues.id_user_customer,
+        user_worker: data.dataValues.id_user_worker,
+        order_status: data.dataValues.id_order_status,
+        order_date: data.dataValues.order_date,
+        courier_name: data.dataValues.courier_name,
+        tracking_code: data.dataValues.tracking_code,
+        start_shipping_date: data.dataValues.start_shipping_date,
+        expected_delivery_date: data.dataValues.expected_delivery_date,
+        delivery_date: data.dataValues.delivery_date,
+        price: data.dataValues.price,
+      };
+      return orderData;
+    });
     //console.log({orderData})
-    return orderData
+    return orderData;
   }
 
   async createOrder(order) {
@@ -50,6 +55,46 @@ export class OrderService {
       return orderList;
     } catch (err) {
       throw new Error(err);
+    }
+  }
+
+  customException = new CustomException();
+  async updateOrderById(id_order, body): Promise<any> {
+    console.log(id_order);
+    console.log(body);
+    await this.customException.checkFindById(id_order, this.OrderRepository);
+
+    return this.OrderRepository.update(
+      {
+        id_user_worker: body.id_user_worker,
+        id_order_status: body.id_order_status,
+      },
+      { where: { id_order: id_order } },
+    ).then((res) => {
+      if (res[0] == 1) {
+        return { result: 'Query executed successfully' };
+      } else {
+        throw new BadRequestException('Something bad happened', {
+          cause: new Error(),
+          description:
+            'Query error, please check your data. Probably, there is no difference between your data and the data that already exists.',
+        });
+      }
+    });
+  }
+}
+
+class CustomException {
+  async checkFindById(id_order, orderRepository) {
+    const arrayIdProducts = await orderRepository
+      .findAll({ attributes: ['id_order'] })
+      .then((res) => res.flatMap((prod) => prod['id_order']));
+
+    if (!arrayIdProducts.includes(Number(id_order))) {
+      throw new NotFoundException('Something bad happened', {
+        cause: new Error(),
+        description: "This id_product doesn't exist in the DB.",
+      });
     }
   }
 }
